0da43b655ef1fb40613c38659906cece45c10d86
Kate Reed <josephreed2600@gmail.com>
2024-04-29 12:34:30 -0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+exports.apply = function(app, component, logger = console.log) {
+	component.routes.forEach((route) => {
+		if(route.method.constructor.name == 'String') route.method = [route.method];
+		route.method.forEach((method) => {
+			if(route.handler.constructor.name == 'Function') route.handler = [route.handler];
+			if(route.method.toLocaleUpperCase() == 'USE') {
+				route.handler.forEach((func) => {
+					logger(`Adding middleware: ${func.name}`);
+					app.use(func);
+				});
+			} else {
+				logger(`Adding route: ${method.toLocaleUpperCase()} ${route.uri} -> ${route.handler.name}`);
+				app[method](route.uri, ...route.handler);
+			}
+		});
+	});
+};
